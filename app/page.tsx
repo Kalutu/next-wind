8e370f42d16e2c6ae591ca43cb9aceae3c32f0bc
Kalutu <kalutudaniel@gmail.com>
2024-03-11 17:24:02 +0300

    Tailwind Typography

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="border-4 border-green-500">Hello world!</h1>
-      <h1 className="text-dan">Hello world!</h1>
+      <h1 className="text-3xl italic underline decoration-red-200">Title</h1>
+      <h2 className="text-2xl">Title 2</h2>
+      <h3 className="text-xl">Title 3</h3>
+      <p className="text-base">Regular paragraph</p>
+      <p className="text-sm">A descrition paragraph</p>
+      <p className="note text-xs">A little note</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,34 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+
+    spacing: {
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+    },
     extend: {
+      fontSize: {
+        xs: "12px",
+        sm: "15px",
+        base: "18px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        dan: "#4a5b67",
+        dan: { 100: "#4a5b67", 200: "#4a5b68", 300: "#4a5b65" },
       },
     },
   },
